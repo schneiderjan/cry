128459b3916a4a46001df11bd87d1b7090af1fef
schneiderjan <heisse.cola@gmail.com>
2014-11-26 10:44:22 +0100

    first commit

---
+++ CRY/Encryption.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CRY
+{
+    class Encryption
+    {
+        private String input, output, cipher;
+
+        public Encryption() { }
+
+        public string CalculateIP(string input)
+        {
+            
+            byte[] bytes = this.ToBinary(input);
+            byte[] bytesAfterIP = new byte[16];
+
+            bytesAfterIP[10] = bytes[0];
+            bytesAfterIP[7] = bytes[1];
+            bytesAfterIP[13] = bytes[2];
+            bytesAfterIP[0] = bytes[3];
+            bytesAfterIP[8] = bytes[4];
+            bytesAfterIP[2] = bytes[5];
+            bytesAfterIP[11] = bytes[6];
+            bytesAfterIP[5] = bytes[7];
+            bytesAfterIP[15] = bytes[8];
+            bytesAfterIP[3] = bytes[9];
+            bytesAfterIP[12] = bytes[10];
+            bytesAfterIP[1] = bytes[11];
+            bytesAfterIP[14] = bytes[12];
+            bytesAfterIP[4] = bytes[13];
+            bytesAfterIP[9] = bytes[14];
+            bytesAfterIP[6] = bytes[15];
+
+            string ipOutput = Encoding.UTF8.GetString(bytesAfterIP, 0, bytesAfterIP.Length);
+            return ipOutput;
+        }
+
+        public byte[] ToBinary(string stringPassed)
+        {
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+            return encoding.GetBytes(stringPassed);
+        }
+
+        
+    }
+}
---
+++ CRY/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CRY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxCipher = new System.Windows.Forms.TextBox();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.labelCipher = new System.Windows.Forms.Label();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelKey = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(88, 30);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // textBoxCipher
+            // 
+            this.textBoxCipher.Location = new System.Drawing.Point(394, 74);
+            this.textBoxCipher.Name = "textBoxCipher";
+            this.textBoxCipher.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCipher.TabIndex = 1;
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(334, 28);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncrypt.TabIndex = 2;
+            this.buttonEncrypt.Text = "Encrypt";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Location = new System.Drawing.Point(419, 28);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.buttonDecrypt.TabIndex = 3;
+            this.buttonDecrypt.Text = "Decrypt";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(548, 30);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOutput.TabIndex = 4;
+            // 
+            // labelCipher
+            // 
+            this.labelCipher.AutoSize = true;
+            this.labelCipher.Location = new System.Drawing.Point(331, 77);
+            this.labelCipher.Name = "labelCipher";
+            this.labelCipher.Size = new System.Drawing.Size(57, 13);
+            this.labelCipher.TabIndex = 5;
+            this.labelCipher.Text = "Cipher text";
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Location = new System.Drawing.Point(48, 33);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(34, 13);
+            this.labelInput.TabIndex = 6;
+            this.labelInput.Text = "Input:";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(500, 33);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(42, 13);
+            this.labelOutput.TabIndex = 7;
+            this.labelOutput.Text = "Output:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(228, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // labelKey
+            // 
+            this.labelKey.AutoSize = true;
+            this.labelKey.Location = new System.Drawing.Point(194, 33);
+            this.labelKey.Name = "labelKey";
+            this.labelKey.Size = new System.Drawing.Size(28, 13);
+            this.labelKey.TabIndex = 9;
+            this.labelKey.Text = "Key:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 192);
+            this.Controls.Add(this.labelKey);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.labelCipher);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.textBoxCipher);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxCipher;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label labelCipher;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelKey;
+    }
+}
+
